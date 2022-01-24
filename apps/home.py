0984618fb7023fea2bd64e4e05340072625f193e
José Beltrán <jose.beltran@aquabiota.se>
2022-01-24 10:22:39 +0100

    adding home app

---
+++ apps/home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+
+def app():
+    st.title("Havsbottens integritet")
+
+    st.markdown(
+        """        
+        """
+    )
+
+    st.info("Klicka på den vänstra sidofältsmenyn för att navigera till de olika apparna.")
+
+    st.subheader("Videotolkningsdata")
+    st.markdown(
+        """
+        
+    """
+    )
